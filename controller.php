ba83f7da38842c51c5276a36dc0d87d73a538780
krsnananda <krsnananda@outlook.com>
2020-02-17 22:39:45 -0300

    - Resolve bugs

--- controller.php
+++ controller.php
@@ -3,12 +3,12 @@
     
     use Moovin\Job\Backend\Model\Account as Account;
 
-    $accounte = new Account();
+    $account = new Account();
     $transferenceAccount = new Account();
 
     use Moovin\Job\Backend\Model\CashDesk as CashDesk;
     $cashDesk = new CashDesk();
-    $saque = 200;
+    $withdraw = 200;
     echo $transferenceAccount->balance = $transferenceAccount->balance + $cashDesk->Transference($withdraw);
 
     echo "<br>" . $account->balance;
